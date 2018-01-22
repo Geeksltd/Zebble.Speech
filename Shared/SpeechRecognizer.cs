1ded3bea254a1f7b5f3c0c526c484d9ed7c18a9b
ms.zojaji <ms.zojaji@gmail.com>
2018-01-22 21:15:14 +0330

    Add SpeechRecognizer

---
+++ Shared/SpeechRecognizer.cs
@@ -0,0 +1,30 @@
+﻿namespace Zebble.Device
+{
+    using System;
+    using System.Threading.Tasks;
+
+    public static partial class SpeechRecognizer
+    {
+        public static Action<string> Listeners;
+
+        public static async Task<bool> Start(Action<string> listener, OnError errorAction = OnError.Alert)
+        {
+            try { await Stop(); } catch { /* No logging is needed. */ }
+
+            try
+            {
+                if (!await Device.Permission.Speech.IsRequestGranted())
+                    throw new Exception("Request was denied to access Speech Recognition.");
+
+                await Thread.UI.Run(DoStart);
+                Listeners += listener;
+                return true;
+            }
+            catch (Exception ex)
+            {
+                await errorAction.Apply(ex);
+                return false;
+            }
+        }
+    }
+}
