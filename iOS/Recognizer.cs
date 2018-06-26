dcc21fa3fe7c0791f6720ea6d76a47f848bfae6c
mszojaji <ms.zojaji@gmail.com>
2018-06-26 16:11:53 +0430

    Fix IOS speech recognizer doubles the text issue

--- iOS/Recognizer.cs
+++ iOS/Recognizer.cs
@@ -10,18 +10,21 @@
     {
         partial class Recognizer
         {
-            static SFSpeechRecognizer recognizer;
             static AVAudioEngine AudioEngine;
+            static SFSpeechRecognizer SpeechRecognizer;
             static SFSpeechAudioBufferRecognitionRequest LiveSpeechRequest;
+            static SFSpeechRecognitionTask RecognitionTask;
+
+            static System.Timers.Timer Timer;
 
             static Task DoStart()
             {
                 if (Device.OS.IsBeforeiOS(10))
                     throw new Exception("This feature is not supported in this device. Please upgrade your iOS.");
 
-                if (recognizer == null)
+                if (SpeechRecognizer == null)
                 {
-                    recognizer = new SFSpeechRecognizer();
+                    SpeechRecognizer = new SFSpeechRecognizer();
                     LiveSpeechRequest = new SFSpeechAudioBufferRecognitionRequest();
                 }
 
@@ -40,21 +43,79 @@
 
             static void StartRecording()
             {
+                NSError error;
+                var audioSession = AVAudioSession.SharedInstance();
+
+                audioSession.SetCategory(AVAudioSessionCategory.Record);
+                audioSession.SetMode(AVAudioSession.ModeMeasurement, out error);
+                audioSession.SetActive(true);
+
+                if (error != null)
+                {
+                    Log.Error(error);
+                    return;
+                }
+
                 AudioEngine = new AVAudioEngine();
                 var node = AudioEngine.InputNode;
-                var recordingFormat = node.GetBusOutputFormat(0);
-                var tapBlock = new AVAudioNodeTapBlock((buffer, when) => { LiveSpeechRequest.Append(buffer); });
-                node.InstallTapOnBus(0, 1024, recordingFormat, tapBlock);
-                AudioEngine.Prepare();
-                AudioEngine.StartAndReturnError(out var error);
 
-                if (error == null)
-                    recognizer.GetRecognitionTask(LiveSpeechRequest,
-                        (SFSpeechRecognitionResult result, NSError err) =>
-                        {
-                            if (err == null)
-                                Listeners?.Invoke(result.BestTranscription.FormattedString);
-                        });
+                LiveSpeechRequest.ShouldReportPartialResults = true;
+
+                RecognitionTask = SpeechRecognizer.GetRecognitionTask(LiveSpeechRequest, (SFSpeechRecognitionResult result, NSError err) =>
+                {
+                    if (err != null)
+                    {
+                        Stop();
+                        Log.Error(err);
+                        return;
+                    }
+
+                    var currentText = result.BestTranscription.FormattedString;
+
+                    if (result.Final)
+                    {
+                        Listeners?.Invoke(currentText);
+                    }
+
+                    Timer = new System.Timers.Timer(20000) { Enabled = true };
+                    Timer.Elapsed += (s, ev) =>
+                    {
+
+                        AudioEngine?.InputNode?.RemoveTapOnBus(0);
+                        AudioEngine?.Stop();
+                        AudioEngine?.Dispose();
+                        AudioEngine = null;
+
+                        LiveSpeechRequest?.EndAudio();
+                        LiveSpeechRequest?.Dispose();
+                        LiveSpeechRequest = null;
+
+                        SpeechRecognizer?.Dispose();
+                        SpeechRecognizer = null;
+
+                        Timer.Stop();
+                        Timer = null;
+
+                        StartRecording();
+                    };
+
+                    Timer.Start();
+                });
+
+                var recordingFormat = node.GetBusOutputFormat(0);
+                node.InstallTapOnBus(0, 1024, recordingFormat, (AVAudioPcmBuffer buffer, AVAudioTime when) =>
+                {
+                    LiveSpeechRequest.Append(buffer);
+                });
+
+                AudioEngine.Prepare();
+                AudioEngine.StartAndReturnError(out error);
+
+                if (error != null)
+                {
+                    Log.Error(error);
+                    return;
+                }
             }
 
             public static Task Stop()
@@ -70,8 +131,11 @@
                 LiveSpeechRequest?.Dispose();
                 LiveSpeechRequest = null;
 
-                recognizer?.Dispose();
-                recognizer = null;
+                SpeechRecognizer?.Dispose();
+                SpeechRecognizer = null;
+
+                Timer?.Dispose();
+                Timer = null;
 
                 return Task.CompletedTask;
             }
